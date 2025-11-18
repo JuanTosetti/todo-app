7384cd96fcad972b84ac35432fd461b0069dd6d4
JuanTosetti <98109884+JuanTosetti@users.noreply.github.com>
2025-11-17 21:02:38 -0300

    Fix: resolver conflicto y finalizar rebase

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let filtroActual = "todas"; // "todas" | "pendientes" | "completadas"
+let ultimoId = 0; //variable global, contador de IDs
 const input = document.getElementById("inputTarea");
 const lista = document.getElementById("listaTareas");
 const form = document.getElementById("formTarea");
@@ -8,34 +10,43 @@ let tareas = [];
 function renderizarTareas() {
 	lista.innerHTML = "";
 
-	tareas.forEach((tarea, index) => {
-		const li = document.createElement("li");
-		li.classList.add("tarea");
-		if (tarea.completada) li.classList.add("completada");
+    let tareasFiltradas = tareas;
 
-		const texto = document.createElement("span");
-		texto.classList.add("tarea-texto");
-		texto.textContent = tarea.texto;
+    if(filtroActual === "pendientes"){
+        tareasFiltradas = tareas.filter(t => !t.completada);
+    } else if(filtroActual === "completada"){
+        tareasFiltradas = tareas.filter(t => t.completada);
+    }
 
-		/* Botón completar */
-		const btnCompletar = document.createElement("button");
-		btnCompletar.classList.add("btn-accion");
-		btnCompletar.innerHTML = tarea.completada
-			? `<i data-lucide="check"></i>`
-			: `<i data-lucide="circle"></i>`;
-		btnCompletar.onclick = () => toggleCompletar(index);
+	tareasFiltradas.forEach(tarea => {
+        const li = document.createElement("li");
+        li.dataset.id = tarea.id;
+        if(tarea.completada) li.classList.add("completada");
 
-		/* Botón borrar */
-		const btnBorrar = document.createElement("button");
-		btnBorrar.classList.add("btn-accion");
-		btnBorrar.innerHTML = `<i data-lucide="trash-2"></i>`;
-		btnBorrar.onclick = () => borrarTarea(index);
+        //boton completar
+        const btnCompletar = document.createElement("button");
+        btnCompletar.classList.add("btn-accion", "btn-completar");
+        btnCompletar.innerHTML = tarea.completada
+            ? `<i data-lucide="check"></i>`
+            : `<i data-lucide="circle"></i>`;
+        
 
-		li.appendChild(texto);
-		li.appendChild(btnCompletar);
-		li.appendChild(btnBorrar);
-		lista.appendChild(li);
-	});
+        //Texto
+        const texto = document.createElement("span");
+        texto.classList.add("tarea-texto");
+        texto.textContent = tarea.texto;
+
+        //Botón borrar
+        const btnBorrar = document.createElement("button");
+        btnBorrar.classList.add("btn-accion","btn-borrar");
+        btnBorrar.innerHTML = `<i data-lucide="trash-2"></i>`;
+
+        li.appendChild(btnCompletar);
+        li.appendChild(texto);
+        li.appendChild(btnBorrar);
+
+        lista.appendChild(li);
+    });
 
 	// IMPORTANTE: activar íconos al renderizar
 	lucide.createIcons();
@@ -46,11 +57,23 @@ function agregarTarea() {
 	const texto = input.value.trim();
 	if (!texto) return;
 
-	tareas.push({ texto, completada: false });
+    const normalizado = texto.toLowerCase().replace(/\s+/g, " ");
+    if(tareas.some(t => t.texto.toLowerCase().replace(/\s+/g, " ") === normalizado)){
+        showToast("Esa tarea ya existe", 2000);
+        return;
+    }
+
+    const nuevaTarea = {
+        id: generarID(),
+        texto,
+        completada: false
+    }
+
+	tareas.push(nuevaTarea);
 	input.value = "";
 
-	renderizarTareas();
 	guardarEnLocalStorage();
+	renderizarTareas();
     actualizarStats();
 	showToast("Tarea agregada", 2000);
 }
@@ -61,19 +84,24 @@ form.addEventListener("submit", e => {
 });
 
 /*BORRAR TAREA*/
-function borrarTarea(index) {
-	tareas.splice(index, 1);
-	renderizarTareas();
+function borrarTarea(id) {
+    tareas = tareas.filter(t => t.id !== id);
+
 	guardarEnLocalStorage();
+	renderizarTareas();
     actualizarStats();
 	showToast("Tarea eliminada", 2000);
 }
 
 /*COMPLETAR TAREA*/
-function toggleCompletar(index) {
-	tareas[index].completada = !tareas[index].completada;
-	renderizarTareas();
+function toggleCompletar(id) {
+    const tarea = tareas.find(t => t.id === id);
+    if(!tarea) return;
+
+	tarea.completada = !tarea.completada;
+
 	guardarEnLocalStorage();
+	renderizarTareas();
     actualizarStats();
 	showToast("Tarea actualizada", 2000);
 }
@@ -81,14 +109,26 @@ function toggleCompletar(index) {
 /* LOCAL STORAGE*/
 function guardarEnLocalStorage() {
 	localStorage.setItem("tareas", JSON.stringify(tareas));
+    localStorage.setItem("ultimoId", JSON.stringify(ultimoId));
 }
 
 function cargarDesdeLocalStorage() {
 	const data = localStorage.getItem("tareas");
+    const idGuardado = JSON.parse(localStorage.getItem("ultimoId"));
+
 	if (data) {
 		tareas = JSON.parse(data);
-		renderizarTareas();
-	}
+	}else{
+        tareas = [];
+    }
+
+    if(typeof idGuardado === "number" && !isNaN(idGuardado)){
+        ultimoId = idGuardado;
+    }else{
+        ultimoId = 0;
+    }
+
+    renderizarTareas();
     actualizarStats();
 }
 
@@ -119,18 +159,53 @@ function actualizarStats(){
     const elTotal = document.getElementById("total");
     const elCompletada = document.getElementById("completadas");
     const elPendientes = document.getElementById("pendientes");
-    
 
-    if(!elTotal || !elCompletada || !elPendientes){
-        return;
-    }
+    if(!elTotal || !elCompletada || !elPendientes){return;}
    
 
-    
+
     elTotal.textContent = total;
     elCompletada.textContent = completadas;
     elPendientes.textContent = pendientes;
 }
+
+function generarID(){
+    return ++ultimoId;
+}
+
+document.getElementById("filtros").addEventListener("click", e =>{
+    if(e.target.matches("button")){
+        filtroActual = e.target.dataset.filtro;
+
+        document.querySelectorAll("#filtros button").forEach(btn => btn.classList.remove("activo"));
+
+        e.target.classList.add("activo");
+
+        renderizarTareas();
+    }
+});
+
+// DELEGACIÓN DE EVENTOS PARA BOTONES DE CADA TAREA
+lista.addEventListener("click", e => {
+
+    const boton = e.target.closest("button");
+    if (!boton) return;
+
+    const li = e.target.closest("li");
+    if (!li) return;
+
+    const id = Number(li.dataset.id);
+
+    // detectar acción según la clase del botón
+    if (boton.classList.contains("btn-completar")) {
+        toggleCompletar(id);
+    }
+
+    if (boton.classList.contains("btn-borrar")) {
+        borrarTarea(id);
+    }
+});
+
 
 
 //Init
